89b1718ba217809b06a14c372b86c7b78c7392ea
Kong Nguyen <shenlongbk@gmail.com>
2015-09-18 10:41:13 +0800

    refactor code

--- BotterNet/Decoders/LoginDecoder.cs
+++ BotterNet/Decoders/LoginDecoder.cs
@@ -13,14 +13,10 @@ namespace BotterNet.Decoder
     {
         public BattleMessage Parse(byte[] content)
         {
-            if (LoginMessage.Length != content.Length)
-                throw new InvalidMessage();
-
             var binReader = new BinaryReader(new MemoryStream(content));
 
             return new LoginMessage() {
-                Username = Encoding.ASCII.GetString(binReader.ReadBytes(LoginMessage.USERNAME_LENGTH)),
-                Password = Encoding.ASCII.GetString(binReader.ReadBytes(LoginMessage.PASSWORD_LENGTH))
+                Username = Encoding.ASCII.GetString(content)
             };
         }
     }
